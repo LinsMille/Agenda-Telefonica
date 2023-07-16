5fb492949cc5443372ccc2313359e4fa8adf1420
Elisangela Bittencourt <121834439+LinsMille@users.noreply.github.com>
2023-07-16 20:10:21 -0300

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,44 @@
+'''
+Agenda simples
+'''
+__author__ = 'Caio César'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+__status__ = 'Development'
+
+from telefone import Telefone
+
+class Controller():
+  def inserir(nome, telefone):
+    return Telefone(nome, telefone)
+
+  def listarAll(listaTelefone):
+    for tel in listaTelefone:
+      print('{} | {}'.format(tel.getNome(), tel.getTelefone()))
+
+  def listarNome(listaTelefone, nome):
+    cont = 0
+    for tel in listaTelefone:
+      if tel.getNome() == nome:
+        print('{} | {}'.format(tel.getNome(), tel.getTelefone()))
+        break
+      cont += 1
+
+  def deletarAll(listaTelefone):
+    if len(listaTelefone) != 0:
+      listaTelefone.clear()
+      return 'Todos os contatos foram removidos!'
+    else:
+      return 'A lista telefonica está vazia!'
+
+  def deletarNome(listaTelefone, nome):
+    if len(listaTelefone) != 0:
+      cont = 0
+      for tel in listaTelefone:
+        if tel.getNome() == nome:
+          listaTelefone.pop(cont)
+          return 'Contado {} removido com sucesso!'.format(nome)
+        else:
+          return 'Nome não encontrado!'
+    else:
+      return 'Lista está vazia!'
